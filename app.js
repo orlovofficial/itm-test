959d1844e4d9256e3c75e98f12cbb8872ec17540
Иван <ivan@MacBook-Air-Ivan.local>
2020-06-05 17:34:59 +0700

    Add routes

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const departmentRoutes = require('./routes/department');
+const employeeRoutes = require('./routes/employee');
 const app = express();
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use('/api/department', departmentRoutes);
+app.use('/api/employee', employeeRoutes);
 
 module.exports = app;
